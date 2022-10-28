b6310ac75e5928ebab1b8523334e939ded2ec873
Poromufflon <mrfishlp@web.de>
2022-10-28 18:14:41 +0200

    build first dao contract on hardhat chain
    i might need to adjust some things
    and setup the project probably to adress
    workflow but for now the basic contracts are atleast deployed

---
+++ deploy/3-deploy-governor-contract.ts
@@ -0,0 +1,34 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { VOTING_DELAY,VOTING_PERIOD,QUORUM_PERCENTAGE } from "../helper-hardhat-config";
+
+
+const deployGovernorContract: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {
+    const {getNamedAccounts,deployments} = hre;
+    const { deploy, log, get} = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const governanceToken = await get("GovernanceToken");
+    const timeLock = await get("TimeLock");
+
+    log("Deploying Governor Contract");
+
+    //Funktioniert nur mit der allowUnlimitedContractSize: true flag in der hardhat config,
+    //wegen https://eips.ethereum.org/EIPS/eip-170
+    const governorContract = await deploy("GovernorContract", {
+        from: deployer,
+        args:[
+            governanceToken.address,
+            timeLock.address,
+            VOTING_DELAY,
+            VOTING_PERIOD,
+            QUORUM_PERCENTAGE,
+        ],
+        log: true,
+        //wait confirmations:
+    });
+};
+
+export default deployGovernorContract
---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import {HardhatUserConfig} from "hardhat/config";
+
+
+
+//module.exports = {
+//  solidity: "0.8.17",
+//};
+
+const config: HardhatUserConfig  = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      chainId: 31337,
+    }
+  },
+  paths:{
+    artifacts: './svelte/src/contracts',
+  },
+  solidity: "0.8.9",
+  namedAccounts: {
+    deployer:{
+      default: 0,
+    },
+  },
+};
+
+export default config;
